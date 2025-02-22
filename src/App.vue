54141b39df1afb570e051a148412a4e2b6e32efc
TheSilkFlower <smirnova100991@gmail.com>
2025-02-23 00:46:58 +0300

    Удалила часть неиспользуемого

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
-<script setup>
-</script>
-
-<template>
-  
+<<template>
+    <h1>
+        Hello, world!
+    </h1>
 </template>
-    <h1>Hello World!</h1>
-<style scoped>
-h1 {
-    font-weight: 25px;
-}
-
-</style>
