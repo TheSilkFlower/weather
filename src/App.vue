8d49db235ed6116ba26ec3717c6fa06433fc82b4
TheSilkFlower <smirnova100991@gmail.com>
2025-03-14 22:21:55 +0300

    Добавила стили для корректного отображения

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <template>
     <div class="main"></div>
     <div class="weather-img">
-        <img src="/winter.png" width="120px" height="auto"/>
+        <img src="/wind.png" width="80px" height="auto" alt="wind"/>
     </div>
 </template>
 
@@ -12,9 +12,7 @@
         height: 100vh;
         position: absolute;
         z-index: -10;
-        background-image: url("/fon.avif");
-        background-repeat: no-repeat;
-        background-size: cover;
+        background: linear-gradient(102deg, rgba(249,255,207,1) 0%, rgba(136,255,209,1) 50%, rgba(41,205,255,1) 100%);
         filter: blur(12px);
     }
     .weather-img {
@@ -23,11 +21,11 @@
         width: 300px;
         height: 400px;
         border-radius: 15px;
-        box-shadow: 0px 0px 5px rgb(78, 78, 244);
+        box-shadow: 0px 0px 5px rgb(98, 100, 92);
         transition: .3s;
     }
     .weather-img:hover {
-        box-shadow: 0px 0px 25px rgb(78, 78, 244);
+        box-shadow: 0px 0px 25px rgb(98, 100, 92);
         cursor: pointer;
     }
 </style>
